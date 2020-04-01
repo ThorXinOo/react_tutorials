648534c37dfd27172c0f88778453f388f1fb267e
Thor Xin Oo <thawzin36978@gmail.com>
2020-04-02 01:29:37 +0630

    First Version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
