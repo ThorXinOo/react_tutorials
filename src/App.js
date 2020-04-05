27a6ac3796e73d65f8d83dd15f47a5bb99e265ea
Thor Xin Oo <thawzin36978@gmail.com>
2020-04-05 23:22:48 +0630

    lesson 14-15 env variable and event and search component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 import axios from 'axios';
 
@@ -14,9 +15,9 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
-
-    console.log(res.data);
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({
       loading: false,
@@ -30,7 +31,9 @@ class App extends Component {
         {/* <Navbar /> */}
         {/* <Navbar title={numbers} icon={numbers} /> */}
         <Navbar title='Github Finder' icon='fab fa-github' />
+
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
